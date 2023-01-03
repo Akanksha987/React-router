74a26cd3d132836bb36ad3cdc254af14a157dfeb
Akanksha Agrawal <akanksha.agrawal@kalvium.community>
2023-01-03 12:58:56 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Navbar from "./Navbar";
+import Contact from "./components/Contact"
+import About from "./components/About"
+import Home from "./components/Home"
+import { Route,Routes } from "react-router-dom";
+function App(){
+  
+  return(
+    <>
+      <Navbar/>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/about" element={<About/>}/>
+          <Route path="/contact" element={<Contact/>}/>
+        </Routes>
+      </div>
+    </>
+    
+  )
 }
-
-export default App;
+export default App
